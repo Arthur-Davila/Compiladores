f88e8943c65d9a95f165f127a5a48039c2bdaed5
nutelinha02 <arthcc@outlook.com>
2024-05-27 17:40:23 +0000

    estudando

---
+++ FuncVar/hello.py
@@ -0,0 +1,17 @@
+#pergunte o nome do usuario
+#str é string
+#Harvard CS50’s Introduction to Programming with Python – Full University Course
+#49:59
+
+name = input("Qual seu nome?\n")
+print ('hello \"friend\"')
+print (f"hello,{name}")
+
+
+#fale o nome da pessoa
+print('hello world '+name)
+print("hello,",name ,sep ='²²² ',end='?')
+#comentario
+"""
+isso é um comentario
+"""
